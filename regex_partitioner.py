cf3925873e33688b41a98b8a7afb7d8d4efae383
Mikel Mcdaniel <mikelmcdaniel@gmail.com>
2022-11-27 12:08:19 -0800

    Use under_scores instead of GoogleStyle for function and method names.

--- regex_partitioner.py
+++ regex_partitioner.py
@@ -16,35 +16,35 @@ import string
 import sys
 
 
-def NumSeqsWithMaxLen(alphabet_size, max_len):
+def num_seqs_with_max_len(alphabet_size, max_len):
   assert max_len >= 0
   return (alphabet_size**(max_len + 1) - 1) // (alphabet_size - 1)
 
 
-def NumToSeq(num, alphabet, max_len):
+def num_to_seq(num, alphabet, max_len):
   assert max_len >= 0
   num = int(num)
   len_seqs_less = max_len - 1
   while num:
-    num_seqs_less = NumSeqsWithMaxLen(len(alphabet), len_seqs_less)
+    num_seqs_less = num_seqs_with_max_len(len(alphabet), len_seqs_less)
     yield alphabet[(num - 1) // num_seqs_less]
     num = (num - 1) % num_seqs_less
     len_seqs_less -= 1
 
 
-def SeqToNum(seq, inverse_alphabet, max_len):
+def seq_to_num(seq, inverse_alphabet, max_len):
   assert max_len >= 0
   len_seq_remaining = max_len - 1
   num = 0
   for c in itertools.islice(seq, 0, max_len):
-    num_seqs_less = NumSeqsWithMaxLen(
+    num_seqs_less = num_seqs_with_max_len(
         len(inverse_alphabet), len_seq_remaining)
     num += 1 + inverse_alphabet[c] * num_seqs_less
     len_seq_remaining -= 1
   return num
 
 
-def TestSeqNum():
+def test_seq_num():
   for alphabet in ('ab', 'abc', 'abcd', 'abcde', 'abcdef'):
     inverse_alphabet = dict((v, k) for k, v in enumerate(alphabet))
     def AllSeqs(alphabet, max_len):
@@ -62,8 +62,8 @@ def TestSeqNum():
     for max_len in range(7):
       all_sequences = sorted(AllSeqs(alphabet, max_len))
       for expected_num, expected_str in enumerate(all_sequences):
-        actual_num = SeqToNum(expected_str, inverse_alphabet, max_len)
-        actual_str = ''.join(NumToSeq(expected_num, alphabet, max_len))
+        actual_num = seq_to_num(expected_str, inverse_alphabet, max_len)
+        actual_str = ''.join(num_to_seq(expected_num, alphabet, max_len))
         try:
           assert expected_num == actual_num
           assert expected_str == actual_str
@@ -85,14 +85,14 @@ class NFA(object):
     self.inverse_alphabet = {}  # {'a': 0, 'b': 1, ..., 'z': 25}
     self._m = {}
 
-  def AddNode(self):
+  def add_node(self):
     new_node_id = len(self.nodes)
     # self.nodes is a default dictionary, so accessing it implicitly
     # creates an entry which can be iterated over.
     self.nodes[new_node_id]  # pylint: disable=pointless-statement
     return new_node_id
 
-  def AddTransition(self, from_node, to_node, transition=None):
+  def add_transition(self, from_node, to_node, transition=None):
     self.nodes[from_node][transition].add(to_node)
     if self._m:
       self._m = {}
@@ -100,14 +100,14 @@ class NFA(object):
       self.inverse_alphabet[transition] = len(self.alphabet)
       self.alphabet.append(transition)
 
-  def PossibleTransitions(self, nodes):
+  def possible_transitions(self, nodes):
     ts = set()
     for node in nodes:
       ts.update(self.nodes[node])
     ts.discard(None)
     return ts
 
-  def _NextNodesSlow(self, cur_nodes, sequence_element):
+  def _next_nodes_slow(self, cur_nodes, sequence_element):
     """Returns the next nodes from the current nodes, given a transition.
 
     Args:
@@ -135,25 +135,25 @@ class NFA(object):
       last_updates = updates
     return frozenset(next_nodes)
 
-  def NextNodes(self, cur_nodes, sequence_element):
+  def next_nodes(self, cur_nodes, sequence_element):
     cur_nodes = frozenset(cur_nodes)
     if (cur_nodes, sequence_element) not in self._m:
-      self._m[cur_nodes, sequence_element] = self._NextNodesSlow(
+      self._m[cur_nodes, sequence_element] = self._next_nodes_slow(
           cur_nodes, sequence_element)
     return self._m[cur_nodes, sequence_element]
 
-  def Accepts(self, sequence):
+  def accepts(self, sequence):
     cur_nodes = self.start_nodes
     for item in sequence:
-      cur_nodes = self.NextNodes(cur_nodes, item)
+      cur_nodes = self.next_nodes(cur_nodes, item)
     return not self.accept_nodes.isdisjoint(cur_nodes)
 
-  def _SumTables(self, table):
+  def _sum_tables(self, table):
     return sum(count
                for nodes, count in table.items()
                if any(node in self.accept_nodes for node in nodes))
 
-  def NumAccepts(self, max_len, bound=()):
+  def num_accepts(self, max_len, bound=()):
     """Returns the number of sequences accepted by this NFA.
 
     Return the number of sequences with length less than or equal to max_len
@@ -179,15 +179,15 @@ class NFA(object):
       has_next = False
     for _ in range(max_len):
       if not has_next:
-        result += self._SumTables(eq1)
-      result += self._SumTables(gt1)
+        result += self._sum_tables(eq1)
+      result += self._sum_tables(gt1)
       for nodes, count in gt1.items():
-        for element in self.PossibleTransitions(nodes):
-          next_nodes = frozenset(self.NextNodes(nodes, element))
+        for element in self.possible_transitions(nodes):
+          next_nodes = frozenset(self.next_nodes(nodes, element))
           gt2[next_nodes] += count
       for nodes, count in eq1.items():
-        for element in self.PossibleTransitions(nodes):
-          next_nodes = frozenset(self.NextNodes(nodes, element))
+        for element in self.possible_transitions(nodes):
+          next_nodes = frozenset(self.next_nodes(nodes, element))
           if not has_next or element > c:
             gt2[next_nodes] += count
           elif element == c:
@@ -200,11 +200,11 @@ class NFA(object):
       except StopIteration:
         has_next = False
     if not has_next:
-      result += self._SumTables(eq1)
-    result += self._SumTables(gt1)
+      result += self._sum_tables(eq1)
+    result += self._sum_tables(gt1)
     return result
 
-  def EnsureDisjoint(self, other_nfa):
+  def ensure_disjoint(self, other_nfa):
     offset = len(other_nfa.nodes)
     self.start_nodes = set(n + offset for n in self.start_nodes)
     self.accept_nodes = set(n + offset for n in self.accept_nodes)
@@ -215,13 +215,13 @@ class NFA(object):
         nodes[node + offset][transition] = set(n + offset for n in next_nodes)
     self.nodes = nodes
 
-  def UpdateNodes(self, other_nfa):
+  def update_nodes(self, other_nfa):
     for k, v in other_nfa.nodes.items():
       self.nodes[k] = v
     self.alphabet = sorted(set(self.alphabet + other_nfa.alphabet))
     self.inverse_alphabet = dict((v, k) for k, v in enumerate(self.alphabet))
 
-  def BuildDotStr(self):
+  def build_dot_str(self):
     """Returns a string representation of this NFA as a GraphViz *.dot file."""
     s = []
     s.append('digraph {')
@@ -246,7 +246,7 @@ class NFA(object):
 class RE(object):
   """Base Class for representing regular expressions."""
 
-  def Match(self, unused_sequence):
+  def match(self, unused_sequence):
     return None
 
   def __repr__(self):
@@ -259,7 +259,7 @@ class RESequence(RE):
   def __init__(self, sequence):
     self.sequence = sequence
 
-  def Match(self, sequence):
+  def match(self, sequence):
     if sequence.startswith(self.sequence):
       return len(self.sequence)
     return None
@@ -270,15 +270,15 @@ class RESequence(RE):
   def __str__(self):
     return str(self.sequence)
 
-  def ToNFA(self):
+  def as_nfa(self):
     """Return the NFA representation of this regular expression."""
     nfa = NFA()
-    prev_node = nfa.AddNode()
+    prev_node = nfa.add_node()
     next_node = prev_node
     nfa.start_nodes.add(prev_node)
     for item in self.sequence:
-      next_node = nfa.AddNode()
-      nfa.AddTransition(prev_node, next_node, item)
+      next_node = nfa.add_node()
+      nfa.add_transition(prev_node, next_node, item)
       prev_node = next_node
     nfa.accept_nodes.add(next_node)
     return nfa
@@ -290,9 +290,9 @@ class REConcat(RE):
   def __init__(self, sub_trees):
     self.sub_trees = list(sub_trees)
 
-  def Match(self, sequence):
+  def match(self, sequence):
     for sub_tree in self.sub_trees:
-      m = sub_tree.Match(sequence)
+      m = sub_tree.match(sequence)
       if m is None:
         return None
       sequence = sequence[m:]
@@ -303,25 +303,25 @@ class REConcat(RE):
   def __str__(self):
     return f'({"".join(str(st) for st in self.sub_trees)})'
 
-  def ToNFA(self):
+  def as_nfa(self):
     if not self.sub_trees:
       # The concatenation of nothing is equal to the empty sequence.
       nfa = NFA()
-      node = nfa.AddNode()
+      node = nfa.add_node()
       nfa.start_nodes.add(node)
       nfa.accept_nodes.add(node)
       return nfa
     else:
-      nfa = self.sub_trees[0].ToNFA()
+      nfa = self.sub_trees[0].as_nfa()
       for sub_tree in itertools.islice(self.sub_trees, 1, len(self.sub_trees)):
-        sub_nfa = sub_tree.ToNFA()
-        sub_nfa.EnsureDisjoint(nfa)
-        nfa.UpdateNodes(sub_nfa)
-        connecting_node = nfa.AddNode()
+        sub_nfa = sub_tree.as_nfa()
+        sub_nfa.ensure_disjoint(nfa)
+        nfa.update_nodes(sub_nfa)
+        connecting_node = nfa.add_node()
         for node in nfa.accept_nodes:
-          nfa.AddTransition(node, connecting_node, None)
+          nfa.add_transition(node, connecting_node, None)
         for node in sub_nfa.start_nodes:
-          nfa.AddTransition(connecting_node, node, None)
+          nfa.add_transition(connecting_node, node, None)
         nfa.accept_nodes = sub_nfa.accept_nodes
       return nfa
 
@@ -332,10 +332,10 @@ class REOr(RE):
   def __init__(self, sub_trees):
     self.sub_trees = list(sub_trees)
 
-  def Match(self, sequence):
+  def match(self, sequence):
     m = None
     for sub_tree in self.sub_trees:
-      m = sub_tree.Match(sequence)
+      m = sub_tree.match(sequence)
       if m is not None:
         break
     return m
@@ -346,13 +346,13 @@ class REOr(RE):
   def __str__(self):
     return f'({"|".join(str(st) for st in self.sub_trees)})'
 
-  def ToNFA(self):
+  def as_nfa(self):
     assert self.sub_trees
-    nfa = self.sub_trees[0].ToNFA()
+    nfa = self.sub_trees[0].as_nfa()
     for sub_tree in itertools.islice(self.sub_trees, 1, len(self.sub_trees)):
-      sub_nfa = sub_tree.ToNFA()
-      sub_nfa.EnsureDisjoint(nfa)
-      nfa.UpdateNodes(sub_nfa)
+      sub_nfa = sub_tree.as_nfa()
+      sub_nfa.ensure_disjoint(nfa)
+      nfa.update_nodes(sub_nfa)
       nfa.start_nodes.update(sub_nfa.start_nodes)
       nfa.accept_nodes.update(sub_nfa.accept_nodes)
     return nfa
@@ -364,13 +364,13 @@ class RERepeat(RE):
   def __init__(self, sub_tree):
     self.sub_tree = sub_tree
 
-  def Match(self, sequence):
+  def match(self, sequence):
     total = 0
-    m = self.sub_tree.Match(sequence)
+    m = self.sub_tree.match(sequence)
     while m is not None:
       total += m
       sequence = sequence[m:]
-      m = self.sub_tree.Match(sequence)
+      m = self.sub_tree.match(sequence)
     return total
 
   def __repr__(self):
@@ -379,18 +379,18 @@ class RERepeat(RE):
   def __str__(self):
     return f'({self.sub_tree})*'
 
-  def ToNFA(self):
-    nfa = self.sub_tree.ToNFA()
-    connecting_node = nfa.AddNode()
+  def as_nfa(self):
+    nfa = self.sub_tree.as_nfa()
+    connecting_node = nfa.add_node()
     for accept_node in nfa.accept_nodes:
-      nfa.AddTransition(accept_node, connecting_node, None)
+      nfa.add_transition(accept_node, connecting_node, None)
     for start_node in nfa.start_nodes:
-      nfa.AddTransition(connecting_node, start_node, None)
+      nfa.add_transition(connecting_node, start_node, None)
     nfa.accept_nodes.update(nfa.start_nodes)
     return nfa
 
 
-def FindPartitionSeq(
+def find_partition_seq(
     nfa, max_len, target_ratio=fractions.Fraction(1, 2),
     lo=(), hi=None, tolerance_ratio=0):
   """Return a sequence that partitions the space of accepted strings.
@@ -413,12 +413,12 @@ def FindPartitionSeq(
   """
 
   max_letter = max(nfa.alphabet)
-  lo = SeqToNum(lo, nfa.inverse_alphabet, max_len)
-  hi = SeqToNum(
+  lo = seq_to_num(lo, nfa.inverse_alphabet, max_len)
+  hi = seq_to_num(
       (max_letter for _ in range(max_len)) if hi is None else hi,
       nfa.inverse_alphabet, max_len)
-  lo_num_accepts = nfa.NumAccepts(max_len, NumToSeq(lo, nfa.alphabet, max_len))
-  hi_num_accepts = nfa.NumAccepts(max_len, NumToSeq(hi, nfa.alphabet, max_len))
+  lo_num_accepts = nfa.num_accepts(max_len, num_to_seq(lo, nfa.alphabet, max_len))
+  hi_num_accepts = nfa.num_accepts(max_len, num_to_seq(hi, nfa.alphabet, max_len))
   total = lo_num_accepts - hi_num_accepts
   assert total >= 0
   target = lo_num_accepts - int(total * target_ratio)
@@ -431,8 +431,8 @@ def FindPartitionSeq(
       assert mid <= hi
     else:
       mid = (lo + hi) // 2
-    mid_str = NumToSeq(mid, nfa.alphabet, max_len)
-    mid_num_accepts = nfa.NumAccepts(max_len, mid_str)
+    mid_str = num_to_seq(mid, nfa.alphabet, max_len)
+    mid_num_accepts = nfa.num_accepts(max_len, mid_str)
     if (lo >= hi or mid_num_accepts == target
         or abs(lo_num_accepts - hi_num_accepts) <= tolerance):
       break
@@ -442,34 +442,34 @@ def FindPartitionSeq(
     elif mid_num_accepts > target:
       lo = mid
       lo_num_accepts = mid_num_accepts
-  return tuple(NumToSeq(mid, nfa.alphabet, max_len))
+  return tuple(num_to_seq(mid, nfa.alphabet, max_len))
 
 
-def FindPartitionSeqs(
+def find_partition_seqs(
     nfa, max_len, num_partitions=1, lo=(), hi=None, tolerance_ratio=0):
   return tuple(
-      FindPartitionSeq(
+    find_partition_seq(
           nfa, max_len,
           fractions.Fraction(j, num_partitions), lo, hi, tolerance_ratio)
-      for j in range(1, num_partitions))
+    for j in range(1, num_partitions))
 
 
 MAX_REPEAT = re.sre_parse.parse('a*')[0][1][1]
 ALPHABET = frozenset(string.printable)
 
 
-def SreToRE(sre, alphabet=ALPHABET):
+def sre_to_re(sre, alphabet=ALPHABET):
   if isinstance(sre, tuple):
     op, args = sre
     op = str(op).lower()
     if op == 'branch':  # or
       none, sres = args
       assert none is None  # I don't know how to interpret a non-None value.
-      res = map(SreToRE, sres)
+      res = map(sre_to_re, sres)
       return REOr(res)
     elif op == 'in':  # or
       sres = args
-      res = map(SreToRE, sres)
+      res = map(sre_to_re, sres)
       return REOr(res)
     elif op == 'category':  # or (\d, \W)
       if args == 'category_digit':
@@ -486,13 +486,13 @@ def SreToRE(sre, alphabet=ALPHABET):
     elif op == 'assert':  # (?=REGEX)
       _, sres = args
       assert len(sres) == 2
-      return SreToRE(sres[1])
+      return sre_to_re(sres[1])
     elif op == 'any':  # .
       return REOr([RESequence(c) for c in alphabet])
     elif op == 'max_repeat' or op == 'min_repeat':  # {m,n}, *, +
       min_repeat, max_repeat, sre = args
       res = []
-      main_res = SreToRE(sre)
+      main_res = sre_to_re(sre)
       if max_repeat >= MAX_REPEAT:
         for _ in range(min_repeat):
           res.append(copy.deepcopy(main_res))
@@ -514,7 +514,7 @@ def SreToRE(sre, alphabet=ALPHABET):
       return REOr([RESequence(c) for c in alphabet if c != literal])
     elif op == 'subpattern':
       _, _, _, sres = args
-      return SreToRE(sres)
+      return sre_to_re(sres)
     elif op == 'range':
       min_literal, max_literal = args
       return REOr([RESequence(chr(c))
@@ -525,15 +525,15 @@ def SreToRE(sre, alphabet=ALPHABET):
       raise Exception(f'Unknown op, args pair: {op!r}, {args!r}')
   else:  # concat
     if len(sre) == 1:
-      return SreToRE(sre[0])
+      return sre_to_re(sre[0])
     else:
-      res = map(SreToRE, sre)
+      res = map(sre_to_re, sre)
       return REConcat(res)
 
 
-def RegexStrToRE(regex_str):
+def regex_str_to_re(regex_str):
   sre = re.sre_parse.parse(regex_str)
-  return SreToRE(sre)
+  return sre_to_re(sre)
 
 
 def main(argv):
@@ -545,11 +545,11 @@ def main(argv):
   tolerance_ratio = 0.25 / num_partitions if len(argv) <= 5 else float(argv[5])
   max_len = 10 if len(argv) <= 6 else int(argv[6])
 
-  t = RegexStrToRE(regex_str)
-  nfa = t.ToNFA()
+  t = regex_str_to_re(regex_str)
+  nfa = t.as_nfa()
 
   print('\n'.join(
-      ''.join(x) for x in FindPartitionSeqs(
+      ''.join(x) for x in find_partition_seqs(
           nfa, max_len, num_partitions, lo, hi, tolerance_ratio)))
 
 
