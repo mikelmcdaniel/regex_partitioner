59e3f8bcbedef02c6551dab66d8df6c4a7b87555
Mikel Mcdaniel <mikelmcdaniel@gmail.com>
2022-12-06 23:13:21 -0800

    Fix bug where non-Sized bounds would fail in NFA.num_accepts

--- regex_partitioner.py
+++ regex_partitioner.py
@@ -162,7 +162,11 @@ class NFA(object):
             result += self._sum_tables(gt2)
             eq1, eq2 = eq2, collections.defaultdict(int)
             gt1, gt2 = gt2, collections.defaultdict(int)
-        return result + (len(bound) <= max_len and self.accepts(bound))
+        try:
+            len_bound: int = len(bound)
+        except TypeError:
+            len_bound = sum(1 for _ in bound)
+        return result + (len_bound <= max_len and self.accepts(bound))
 
     def ensure_disjoint(self, other_nfa: "NFA") -> None:
         offset = len(other_nfa.nodes)
